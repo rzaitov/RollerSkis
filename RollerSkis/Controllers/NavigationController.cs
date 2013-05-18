47a3beb48dd9ea0832780751b39b7c6467b91f3d
rzaitov <r-zaitov@yandex.ru>
2013-05-19 01:46:01 +0300

    Начал реализацию контрола отображения меню

---
+++ RollerSkis/Controllers/NavigationController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Logic;
+using Logic.Domain;
+using Logic.Service;
+
+using RollerSkis.Models;
+
+namespace RollerSkis.Controllers
+{
+	public class NavigationController : Controller
+	{
+		private static readonly ProductServices ProductServices;
+
+		static NavigationController ()
+		{
+			ProductServices = ApplicationContext.ProductService;
+		}
+
+		public PartialViewResult Menu ()
+		{
+
+			IEnumerable<ProductTypeNode> hierarchy = ProductServices.GetProductTypesHierarchy ();
+			return PartialView ("Menu", hierarchy);
+		}
+	}
+}
