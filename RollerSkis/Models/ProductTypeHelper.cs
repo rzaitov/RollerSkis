b5f412b6d84116fd6b4d2a6e1f96dd6713e75723
rzaitov <r-zaitov@yandex.ru>
2013-05-18 23:41:39 +0300

    Добавил класс помощник который содержит в себе словарь имен типов товаров

---
+++ RollerSkis/Models/ProductTypeHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Logic.Domain;
+
+namespace RollerSkis.Models
+{
+	public class ProductTypeHelper
+	{
+		/*
+		private class TypeMap
+		{
+			public ProductType? Parent { get; set; }
+			public ProductType ProductType { get; set; }
+		}
+
+		private class NameMap
+		{
+			public ProductType? Parent { get; set; }
+			public string TypeName { get; set; }
+		}
+		*/
+		private static Dictionary<string, ProductType> productNameTypesMap;
+		private static Dictionary<ProductType, string> productTypesNameMap;
+
+		static ProductTypeHelper ()
+		{
+			productNameTypesMap = new Dictionary<string, ProductType> ();
+			productTypesNameMap = new Dictionary<ProductType, string> ();
+
+			AddToMap (ProductType.RollerSkis, "roller-skis");
+				AddToMap (ProductType.Skate, "skate");
+				AddToMap (ProductType.Classic, "classic");
+				AddToMap (ProductType.Combi, "combi");
+
+			AddToMap (ProductType.Accessories, "accessories");
+			AddToMap (ProductType.Accessories, "nordixc");
+			AddToMap (ProductType.Brakes, "brakes");
+			AddToMap (ProductType.Brakes, "speed-reducers");
+		}
+
+		static void AddToMap (ProductType type, string typeName)
+		{
+			productNameTypesMap[typeName] = type;
+			productTypesNameMap[type] = typeName;
+		}
+
+		public ProductType this[string key]
+		{
+			get { return productNameTypesMap[key]; }
+		}
+
+		public string this[ProductType key]
+		{
+			get { return productTypesNameMap[key]; }
+		}
+	}
+}
