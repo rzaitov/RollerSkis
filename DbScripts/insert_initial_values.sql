d1944701da8969e579cc3d8ff2fdfdc550f0f55c
rzaitov <r-zaitov@yandex.ru>
2013-05-21 01:51:17 +0300

    добавил больше товаров в тестовую БД, чтобы интереснее было смотреть на сайт

--- DbScripts/insert_initial_values.sql
+++ DbScripts/insert_initial_values.sql
@@ -2,7 +2,9 @@ INSERT INTO ProductType (parent_id, name)
 VALUES
 	(NULL, 'Speed Reducers'),
 	(NULL, 'Brakes'),
+	(NULL, 'Wheels'),
 	(NULL, 'Accessories'),
+	(NULL, 'Nordixc'),
 	(NULL, 'Roller skis')
 	
 DECLARE @LastId INT;
